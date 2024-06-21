4f9592745811ec076e43388cb6434abc031436b1
sarathmeridian <sarath.k@meridian.net.in>
2024-06-21 14:46:59 +0530

    initial commit

---
+++ app/src/main/java/com/example/api/ui/activity/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.api.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.api.R
+import com.example.api.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private var binding: ActivitySplashBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+         setContentView(binding?.root)
+
+    }
+}
